f133d63b377dcccdf6e08edc1b80f2bc4c252a57
ahmad200202 <139365763+ahmad200202@users.noreply.github.com>
2023-08-08 12:08:33 -0400

    Add files via upload

---
+++ modules/angular-ui-tree/examples/js/table-example.js
@@ -0,0 +1,1 @@
+!function(){"use strict";angular.module("demoApp").controller("TableExampleCtrl",["$scope",function($scope){$scope.remove=function(scope){scope.remove()},$scope.newItem=function(){$scope.data[$scope.data.length-1];$scope.data.push({id:$scope.data.length+1,title:"node "+($scope.data.length+1)})},$scope.data=[{id:1,title:"node 1"},{id:2,title:"node 2"},{id:3,title:"node 3"}]}])}();
