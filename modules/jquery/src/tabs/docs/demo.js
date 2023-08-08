f90a22929f50f45f89f25d15e6626bca7de1f8fc
ahmad200202 <139365763+ahmad200202@users.noreply.github.com>
2023-08-08 12:43:21 -0400

    Add files via upload

---
+++ modules/jquery/src/tabs/docs/demo.js
@@ -0,0 +1,1 @@
+angular.module("ui.bootstrap.demo").controller("TabsDemoCtrl",function($scope,$window){$scope.tabs=[{title:"Dynamic Title 1",content:"Dynamic content 1"},{title:"Dynamic Title 2",content:"Dynamic content 2",disabled:!0}],$scope.alertMe=function(){setTimeout(function(){$window.alert("You've selected the alert tab!")})},$scope.model={name:"Tabs"}});
