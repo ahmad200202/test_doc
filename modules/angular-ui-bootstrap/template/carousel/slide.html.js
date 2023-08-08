c88ecf9dd4c5db94f1b496212c75eab4b59e8026
ahmad200202 <139365763+ahmad200202@users.noreply.github.com>
2023-08-08 12:29:45 -0400

    Add files via upload

---
+++ modules/angular-ui-bootstrap/template/carousel/slide.html.js
@@ -0,0 +1,1 @@
+angular.module("uib/template/carousel/slide.html",[]).run(["$templateCache",function($templateCache){$templateCache.put("uib/template/carousel/slide.html",'<div class="text-center" ng-transclude></div>\n')}]);
---
+++ modules/angular-ui-bootstrap/template/progressbar/progress.html.js
@@ -0,0 +1,1 @@
+angular.module("uib/template/progressbar/progress.html",[]).run(["$templateCache",function($templateCache){$templateCache.put("uib/template/progressbar/progress.html",'<div class="progress" ng-transclude aria-labelledby="{{::title}}"></div>')}]);
